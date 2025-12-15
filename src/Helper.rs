c6049262f70fa910058a8dd4bdb4d613e678e687
IhitplayzYT <ihitexe@gmail.com>
2025-12-15 21:51:42 +0530

    Compiler project on my most recently learned language Rust

--- src/Helper.rs
+++ src/Helper.rs
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// Copyright (C) 2025 Ihit Acharya
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+
 // Helper.rs //
 // Provides some required basic helper functions.
 
