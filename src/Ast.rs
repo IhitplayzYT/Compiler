c34eab1ef8f8ebba019ed6b4786d2330bfa71cf2
IhitplayzYT <ihitexe@gmail.com>
2025-12-26 13:26:59 +0530

    Implementing Pratt's parser to eval expr and statmnt

--- src/Ast.rs
+++ src/Ast.rs
@@ -1,7 +1,8 @@
 pub mod AST {
     use crate::Lexer_Tok::Lex_Tok::LTOK;
-    use std::{cell::RefCell, rc::Rc};
-    pub type link = Rc<RefCell<AstNode>>;
+    use std::{cell::{Ref, RefCell}, rc::Rc};
+    pub type link<T> = Rc<RefCell<T>>;
+
     pub enum Expr{
         Int(i64),
         Float(f64),
@@ -9,33 +10,50 @@ pub mod AST {
         Null,
         Ident(String),
         Binary_op {
-            op: LTOK, // Represents the Node for an operator
-            left: link,  // Unary ops such as shorthands can be represented by considering the left pointer ab the variable on which the unary op being done
-            right: link,
+            op: link<Expr>, // Represents the Node for an operator
+            left: link<Expr>,  // Unary ops such as shorthands can be represented by considering the left pointer ab the variable on which the unary op being done
+            right: link<Expr>
         },
+        Unary_op{
+            op:LTOK,
+            operand: link<Expr>,
+        }
     }
 
-    pub enum AstNode {
+    pub enum Statmnt {
         Expression(Expr),
         Let {
             name: String, // Deals with let,mut and const
             mutable: bool,
-            value: link,
+            value: link<Expr>,
         },
         If {
-            cond: link,
-            then_branch: Vec<link>, // Deals with if and else
-            else_branch: Option<Vec<link>>,
+            cond: link<Expr>,
+            then_branch: Vec<link<Statmnt>>, // Deals with if and else
+            else_branch: Option<Vec<link<Statmnt>>>,
         },
         While {
-            cond: link, // While loop
-            body: Vec<link>,
+            cond: link<Expr>, // While loop
+            body: Vec<link<Statmnt>>,
         },
         For {
-            init: Vec<link>,
-            cond: Option<link>, // For loop SYNTAX : C and Rust hybrid type
-            step: Option<link>,
-            body: Vec<link>,
+            init: Vec<link<Expr>>,
+            cond: Option<link<Expr>>, // For loop SYNTAX : C and Rust hybrid type
+            step: Option<link<Expr>>,
+            body: Vec<link<Statmnt>>,
         },
+    }
+    pub enum Code{
+        Expr,
+        Statmnt,
+    }
+    pub struct AST_Node{
+        code : Code,
+        children : (Option<link<AST_Node>>,Option<link<AST_Node>>),
+    }
+    impl AST_Node{
+        fn new(c : Code) -> Self{
+       Self {code:c,children:(None,None)} 
+    }
     }
 }
--- src/Parser.rs
+++ src/Parser.rs
@@ -1,25 +1,30 @@
 pub mod PARSER {
     use std::{collections::HashMap, hash::Hash};
     use once_cell::sync::Lazy;
-    use crate::{Ast::{self, AST::{self, link}}, Lexer_Tok::Lex_Tok::LTOK};
+    use crate::{Ast::{self, AST::{self,AST_Node,link,Statmnt,Expr,Code}}, Lexer_Tok::Lex_Tok::LTOK};
     pub static Presced : Lazy<HashMap<LTOK,(f32,f32)>> = Lazy::new(|| {HashMap::from([
         (LTOK::PLUS,(2.0,1.9)),(LTOK::DIV,(3.2,3.1)),(LTOK::MINUS,(2.2,2.1)),(LTOK::STAR,(3.0,2.9)),(LTOK::BANG,(-1.0,2.8)),(LTOK::LPAREN,(0.0,10.0)),(LTOK::RPAREN,(10.0,0.0)),(LTOK::AMP,(1.2,1.15)),(LTOK::PIPE,(1.1,1.0)),(LTOK::CARET,(1.3,1.25)),(LTOK::ANDAND,(0.2,0.15)),(LTOK::OROR,(0.1,0.05)),(LTOK::TILDA,(0.0,1.4)),(LTOK::ASSGN,(0.0,0.0)),(LTOK::EQ,(0.95,0.9)),(LTOK::N_EQ,(0.96,0.91)),(LTOK::GT,(0.97,0.92)),(LTOK::LT,(0.98,0.93)),(LTOK::LT_EQ,(0.99,0.94)),(LTOK::GT_EQ,(0.995,0.945))
         ])});
     pub struct Parser {
         input: Vec<LTOK>,
         idx:usize,
-        ast: Option<AST::AstNode>,
+        ast: Option<AST::AST_Node>,
     }
     impl Parser{
         fn new(v:Vec<LTOK>) -> Self{
             Self{ast:None,input:v,idx:0}
         }
-
         fn next(&mut self) -> &LTOK{
+            if self.input.is_empty() {
+                return &LTOK::EOF;
+            }
             self.idx += 1;
             self.input.get(self.idx - 1).unwrap_or(&LTOK::EOF)
         }
         fn peek(&self) -> &LTOK{
+            if self.input.is_empty() {
+                return &LTOK::EOF;
+            }
             self.input.get(self.idx).unwrap_or(&LTOK::EOF)
         }
         fn expect(&mut self,e: LTOK) -> bool{
@@ -29,15 +34,33 @@ pub mod PARSER {
             false
         }
 
-        fn Parse(&mut self) -> bool {            
-            let mut ret:Option<AST::AstNode> = None;
-            for i in &self.input{
-                match *i{
-                    LTOK::EOF => {break;},
-                   _ => {return false;}
-                }
-
+        fn binding_pow(tok : &LTOK) -> (i32,i32){ 
+            match *tok {
+                LTOK::DIV | LTOK::STAR => (60,61),
+                LTOK::PLUS | LTOK::MINUS => (50,51),
+                LTOK::LSHIFT | LTOK::RSHIFT => (45,46),
+                LTOK::AMP => (40, 41),
+                LTOK::CARET => (39, 40),
+                LTOK::PIPE => (38, 39),
+                LTOK::EQ | LTOK::N_EQ | LTOK::GT | LTOK::GT_EQ | LTOK::LT_EQ | LTOK::LT => (30, 31),
+                LTOK::ANDAND => (20, 21),
+                LTOK::OROR => (10, 11),
+                LTOK::S_AMP | LTOK::S_PIPE | LTOK::S_CARET |LTOK::S_PLUS |LTOK::S_MULT |LTOK::S_MINUS |LTOK::S_DIV |LTOK::S_MOD => (0,1),
+                _ => (-1,-1),
             }
+
+        }
+
+
+        fn Parse(&mut self) -> bool {          
+            if self.input.is_empty(){
+                return false;
+            }
+            let mut ret: Option<AST_Node> = None;
+            
+        
+
+
 
             self.ast = ret;
             true
@@ -45,7 +68,5 @@ pub mod PARSER {
 
 
     }
-
-
 
 }
